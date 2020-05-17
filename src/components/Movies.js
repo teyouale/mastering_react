2f89dae58ff0b8da3097cb9fc87c71b5d4655b0d
eyouale <51370727+eyouale@users.noreply.github.com>
2020-05-17 12:18:11 +0300

    Making Movies Components and Deleteing Function

---
+++ src/components/Movies.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+
+class Movies extends Component {
+  state = {
+    movies: getMovies()
+  };
+
+  handleDelete = movie => {
+    const movies = this.state.movies.filter(m => m._id !== movie._id);
+    this.setState({ movies: movies });
+  };
+
+  render() {
+    if (this.state.movies.length !== 0)
+      return (
+        <div className='container'>
+          <p style={{ margin: 10 }}>
+            Showing {this.state.movies.length} in the database
+          </p>
+          <table className='table'>
+            <thead>
+              <tr>
+                <th scope='col'>Title</th>
+                <th scope='col'>Genre</th>
+                <th scope='col'>Stock</th>
+                <th scope='col'>Rate</th>
+                <th scope='col' />
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.movies.map(movie => (
+                <tr key={movie._id}>
+                  <td>{movie.title}</td>
+                  <td>{movie.genre.name}</td>
+                  <td>{movie.numberInStock}</td>
+                  <td>{movie.dailyRentalRate}</td>
+                  <td>
+                    <button
+                      className='btn btn-danger'
+                      onClick={() => this.handleDelete(movie)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      );
+    else
+      return (
+        <h2 className='container' style={{ margin: 10, textAlign: "center" }}>
+          No Movies Database
+        </h2>
+      );
+  }
+}
+
+export default Movies;
